2a00151c9724ee8b049dafffb48c28dce417e8e8
xiet26 <dangcuongbui@ctigroupjsc.com>
2025-08-24 14:54:27 +0700

    [UPDATE] sale_order_cost_management: Add total cost computation and update Sale Order view to display costs

--- sale_order_cost_management/models/sale_order.py
+++ sale_order_cost_management/models/sale_order.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     cost_ids = fields.One2many('sale.order.cost', 'order_id', string='Costs')
+    total_cost = fields.Monetary(string='Total Cost', compute='_compute_total_cost', store=True)
+
+    @api.depends('cost_ids.amount')
+    def _compute_total_cost(self):
+        for order in self:
+            order.total_cost = sum(order.cost_ids.mapped('amount'))
