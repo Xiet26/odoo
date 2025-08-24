e288342063b6ceddbb5684fafa16cec720f821d4
xiet26 <dangcuongbui@ctigroupjsc.com>
2025-08-24 16:38:02 +0700

    [UPDATE] sale_order_cost_management: Add estimated profit computation and display in Sale Order view

--- sale_order_cost_management/models/sale_order.py
+++ sale_order_cost_management/models/sale_order.py
@@ -8,7 +8,14 @@ class SaleOrder(models.Model):
     cost_ids = fields.One2many('sale.order.cost', 'order_id', string='Costs')
     total_cost = fields.Monetary(string='Total Cost', compute='_compute_total_cost', store=True)
 
+    estimated_profit = fields.Monetary(string='Estimated Profit', compute='_compute_estimated_profit', store=True)
+
     @api.depends('cost_ids.amount')
     def _compute_total_cost(self):
         for order in self:
             order.total_cost = sum(order.cost_ids.mapped('amount'))
+
+    @api.depends('amount_total', 'total_cost')
+    def _compute_estimated_profit(self):
+        for order in self:
+            order.estimated_profit = order.amount_total - order.total_cost
